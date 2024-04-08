f5f6a8d5ba0a994dca23101d40670bb4c8ee1808
Lenox Miheso <lenoxmiheso@gmail.com>
2024-04-08 23:38:42 +0300

    modified:   src/userGreeting.jsx

--- src/userGreeting.jsx
+++ src/userGreeting.jsx
@@ -1,6 +1,20 @@
+import propTypes from 'prop-types';
+
 
 function userGreeting(props) {
     
     return (props.isLoggedIn ? <h1>Welcome back, {props.username}!</h1> : <h1>Welcome, Guest!</h1>)
 }
+
+userGreeting.propTypes = {
+    isLoggedIn: propTypes.bool.isRequired,
+    username: propTypes.string.isRequired
+}
+
+userGreeting.defaultProps = {
+    isLoggedIn: false,
+    username: "Guest"
+}
+
 export default userGreeting
+
